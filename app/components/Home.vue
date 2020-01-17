63b48c84a44fe8d10db06a6f3edca05c7a243255
Benjamin Franz <bennyalex98@gmail.com>
2020-01-17 17:26:07 +0100

    First commit

---
+++ app/components/Home.vue
@@ -0,0 +1,31 @@
+/**
+ * Home
+ */
+
+<style lang="stylus" scoped>
+
+.home
+	padding 20px
+	color blue
+
+</style>
+
+<template lang="pug">
+
+.home
+
+	p {{ text }}
+
+	router-link(to="/section") Go to Section
+
+</template>
+
+<script>
+
+export default {
+
+	name: "Home",
+
+	data: () => ({ text: "Hello World from Home!" })
+}
+</script>
