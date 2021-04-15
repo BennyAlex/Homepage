a527c823ff70488b175bab713d643ff5ca1d845b
bfranz <b.franz@bueroparallel.de>
2021-04-15 16:14:24 +0200

    Fix public path

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,16 @@
 module.exports = {
     outputDir: 'docs',
-    publicPath: './',
-
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/Homepage/'
+        : '/',
+    
     css: {
         loaderOptions: {
             sass: {
                 webpackImporter: false,
                 sassOptions: {
                     includePaths: ['./node_modules']
-                },
+                }
             }
         }
     }
